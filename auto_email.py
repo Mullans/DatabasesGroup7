ccf841d1f4b61e37f4033f54a61eedefedccdd21
jjtakes <jj.takes@gmail.com>
2018-12-03 14:14:30 -0600

    Delete auto_email.py

--- auto_email.py
+++
@@ -1,8 +0,0 @@
-def send_simple_message():
-    return requests.post(
-        "https://api.mailgun.net/v3/sandbox21492247c0a34558a909eca1d73200eb.mailgun.org/messages",
-        auth=("api", "8571c86055f9c0fb4a572437d21fea58-059e099e-386b6281"),
-        data={"from": "Mailgun Sandbox <postmaster@sandbox21492247c0a34558a909eca1d73200eb.mailgun.org>",
-              "to": "jonathon takes <jonathon-takes@uiowa.edu>",
-              "subject": "Hello jonathon takes",
-              "text": "Congratulations jonathon takes, you just sent an email with Mailgun!  You are truly awesome!"})
